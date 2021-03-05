a0e5e47d7c11c1264bfff33e1c6fc0450bbca13c
LeonardoW <68288212+LeonardoW01@users.noreply.github.com>
2021-03-05 12:18:19 +0700

    upbaru

---
+++ Aplikasi Persawaan VCD/src/models/Detail.java
@@ -0,0 +1,41 @@
+package models;
+
+public class Detail {
+    private String id;
+    private String namaUser;
+    private String judul;
+    private String tgl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNamaUser() {
+        return namaUser;
+    }
+
+    public void setNamaUser(String namaUser) {
+        this.namaUser = namaUser;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getTgl() {
+        return tgl;
+    }
+
+    public void setTgl(String tgl) {
+        this.tgl = tgl;
+    }
+    
+}
